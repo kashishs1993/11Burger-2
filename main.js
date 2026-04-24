c0f170f53e2b1ee4fcfd7cf532377b4c36506096
kashishs1993 <kashishs1993@gmail.com>
2026-04-24 11:25:56 +0200

    target file

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
       const mindarThree = new window.MINDAR.IMAGE.MindARThree({
         container: document.body,
-        imageTargetSrc: './images/target/französischer_truffle_smash.mind',
+        imageTargetSrc: './targets.mind',
         uiScanning: "no",
         uiLoading: "no",
       });
